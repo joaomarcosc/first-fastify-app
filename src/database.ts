9c65cbd192a0ab9d73cabc2d09a84f2eeda7abc1
joaomarcos <j.marcos9@hotmail.com>
2024-02-24 01:16:29 -0300

    chore: add connectionstring on knex config

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 const connection =
   env.DATABASE_CLIENT === 'sqlite'
     ? { filename: env.DATABASE_URL }
-    : env.DATABASE_URL
+    : { connectionString: env.DATABASE_URL }
 
 export const config: Knex.Config = {
   client: 'sqlite',
